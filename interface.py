50963298ea426f657801330d2f5aa015d207113c
victor-gabriel-b <53690574+victor-gabriel-b@users.noreply.github.com>
2020-08-14 22:31:58 -0300

    UPDATE interface.py
    Incluir avisos de copyright

--- interface.py
+++ interface.py
@@ -1,3 +1,18 @@
+# ArpInspect: Um programa de segurança de redes internas contra ataques ARP Spoof
+#Copyright (C) 2020  Victor Gabriel Batista Reinaldo, Edclaudio Santos de araújo
+
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# Informações de contato:
+# Victor: victor.reinaldo@academico.ifpb.edu.br
+# Edclaudio: edclaudio.santos@academico.ifpb.edu.br
+
 import subprocess
 
 caminho_senha = "/etc/arpdef/passwd"
