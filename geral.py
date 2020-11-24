9d7c2285a21c949f85ace611c204534cc5d5b4e6
repl.it user <replituser@example.com>
2020-11-24 21:36:36 +0000

    Consertar config block_arp_grat

--- geral.py
+++ geral.py
@@ -87,7 +87,7 @@ def rodar(comando):
 
 # Cria o arquivo de configuração com suas configurações padrão
 def inicializar_config():
-  criar_arquivo(caminho_conf, "#Tempo de cada ciclo de execução\ntempo=60\n#Quantidade de pacotes por ciclo de execução que é considerada como um ataque\nqtd=5\n#Configura o mac do gateway. Você pode deixar como auto pra obter autromaticamente. (É fortemente recomendado setar manualmente essa configuração)\nmac_gateway=auto\n#Seta de forma fixa o ip do gateway. Você pode deixar como auto pra obter automaticamente.\nip_gateway=auto\n#Ativa ou desativa o bloqueio de ARP Replies gratuitos (true ou false).\nblock_arp_grat=true\n#Email que vai ser usado pra enviar notificações\nemail=")
+  criar_arquivo(caminho_conf, "#Tempo de cada ciclo de execução\ntempo=60\n#Quantidade de pacotes por ciclo de execução que é considerada como um ataque\nqtd=5\n#Configura o mac do gateway. Você pode deixar como auto pra obter autromaticamente. (É fortemente recomendado setar manualmente essa configuração)\nmac_gateway=auto\n#Seta de forma fixa o ip do gateway. Você pode deixar,0 como auto pra obter automaticamente.\nip_gateway=auto\n#Ativa ou desativa o bloqueio de ARP Replies gratuitos (true ou false).\nblock_arp_grat=true\n#Email que vai ser usado pra enviar notificações\nemail=")
 
 # Realiza a configuração do bloqueio de arp replies gratuitos
 def config_block_arp_grat(valor):
@@ -159,7 +159,7 @@ def editar_config_gui():
     try:
       if config_alterada[1] == "block_arp_grat":
         print(config_alterada) # Testar ***
-        config_block_arp_grat(config_alterada[1]) #   Testar
+        config_block_arp_grat(config_alterada[0]) #   Testar
     except IndexError:
       pass
       
